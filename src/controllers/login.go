feb1b3fc5946855de8f72d9738af8ab1ef64a10b
ThomasITU <thwr@itu.dk>
2022-02-19 10:24:37 +0000

    Integrate BasicAuth into login post

--- src/controllers/login.go
+++ src/controllers/login.go
@@ -5,33 +5,15 @@ import (
 	_ "log"
 	"net/http"
 
-	pwdHash "github.com/Devops-2022-Group-R/itu-minitwit/src/password"
 	. "github.com/Devops-2022-Group-R/itu-minitwit/src/database"
+	pwdHash "github.com/Devops-2022-Group-R/itu-minitwit/src/password"
 	"github.com/gin-gonic/gin"
 )
 
+// This is not needed when we use BasicAuth ?
 type Login struct {
 	Username string `form:"username" json:"user" binding:"required"`
 	Password string `form:"password" json:"password" binding:"required"`
-}
-
-// Logs the user in.
-func LoginPost(c *gin.Context) {
-	var body Login
-	if c.BindJSON(&body) == nil {
-		db := c.MustGet("db").(*sql.DB)
-		users := QueryDb(db, "select * from user where username = ?", body.Username)
-
-		if len(users) == 0 {
-			c.JSON(http.StatusNotFound, gin.H{"status": "Invalid username"})
-		} else if !pwdHash.CheckPasswordHash(body.Password, users[0]["pw_hash"].(string)) {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "password is incorrect"})
-		} else {
-			//TODO: add login logic instead of session - gin authentication?
-			c.JSON(http.StatusNoContent, nil)
-			return
-		}
-	}
 }
 
 // CREDENTIALS = ':'.join([USERNAME, PWD]).encode('ascii')
@@ -40,17 +22,26 @@ func LoginPost(c *gin.Context) {
 //            'Content-Type': 'application/json',
 //            f'Authorization': f'Basic {ENCODED_CREDENTIALS}'}
 
-func basicAuth(c *gin.Context) {
-	// Get the Basic Authentication credentials
-	user, password, hasAuth := c.Request.BasicAuth()
 
-	if hasAuth && user == "testuser" && password == "testpassword" {
-		log.WithFields(log.Fields{
-			"user": user,
-		}).Info("User authenticated")
-	} else {
-		c.Abort()
-		c.Writer.Header().Set("WWW-Authenticate", "Basic realm=Restricted")
-		return
+// Logs the user in.
+func LoginPost(c *gin.Context) { // add bool return type?
+	var body Login // remove this?
+	if c.BindJSON(&body) == nil {
+		db := c.MustGet("db").(*sql.DB)
+		username, password, hasAuth := c.Request.BasicAuth()
+		users := QueryDb(db, "select * from user where username = ?", username)
+
+		if len(users) == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"status": "Invalid username"})
+		} else if !pwdHash.CheckPasswordHash(password, users[0]["pw_hash"].(string)) {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "password is incorrect"})
+		} else if hasAuth {
+			c.JSON(http.StatusNoContent, nil)
+			return
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "Couldn't authenticate"}) // // c.Writer.Header().Set("WWW-Authenticate", "Basic realm=Restricted")
+			c.Abort()
+			return
+		}
 	}
 }
