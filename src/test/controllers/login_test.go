4615bf7dd4adc09fee1d915a16ddbfe17afad598
ThomasITU <thwr@itu.dk>
2022-02-19 16:54:19 +0000

    add first loginController tests

---
+++ src/test/controllers/login_test.go
@@ -0,0 +1,35 @@
+package controllers_test
+
+import (
+	"encoding/base64"
+	"net/http"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func (suite *TestSuite) TestLoginController_GivenNoBody_Returns401() {
+	req, _ := http.NewRequest(http.MethodGet, "/login", nil)
+	w := suite.sendRequest(req)
+
+	assert.Equal(suite.T(), http.StatusUnauthorized, w.Code)
+}
+
+func (suite *TestSuite) TestLoginController_Given_Bruce_Lee_10000kicks_returns404() {
+	req, _ := http.NewRequest(http.MethodPost, "/login", nil)
+	encodedCredentials := encodeCredentialsTob64("Bruce Lee", "10000kicks")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Connection", "close")
+	req.Header.Set("Authorization", "Basic "+encodedCredentials)
+
+	w := suite.sendRequest(req)
+
+	assert.Equal(suite.T(), http.StatusNotFound, w.Code)
+}
+
+
+// helper
+func encodeCredentialsTob64(username string, password string) string {
+	data := username + ":" + password
+	sEnc := base64.StdEncoding.EncodeToString([]byte(data))
+	return sEnc
+}
