eb5504506ab2a42849ef7e96df6a936208614eb9
ThomasITU <thwr@itu.dk>
2022-02-19 10:41:48 +0000

    Add IsAuthenticated

--- src/controllers/login.go
+++ src/controllers/login.go
@@ -10,38 +10,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// This is not needed when we use BasicAuth ?
-type Login struct {
-	Username string `form:"username" json:"user" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
+// Logs the user in.
+func LoginPost(c *gin.Context) {
+	db := c.MustGet("db").(*sql.DB)
+
+	username, password, hasAuth := c.Request.BasicAuth()
+	if !hasAuth {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "Couldn't authenticate"})
+		return
+	}
+	users := QueryDb(db, "select * from user where username = ?", username)
+
+	if len(users) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"status": "Invalid username"})
+		return
+	} else if !pwdHash.CheckPasswordHash(password, users[0]["pw_hash"].(string)) {
+		c.JSON(http.StatusUnauthorized, gin.H{"status": "password is incorrect"})
+		return
+	}
+	c.JSON(http.StatusNoContent, nil)
 }
 
-// CREDENTIALS = ':'.join([USERNAME, PWD]).encode('ascii')
-// ENCODED_CREDENTIALS = base64.b64encode(CREDENTIALS).decode()
-// HEADERS = {'Connection': 'close',
-//            'Content-Type': 'application/json',
-//            f'Authorization': f'Basic {ENCODED_CREDENTIALS}'}
 
-
-// Logs the user in.
-func LoginPost(c *gin.Context) { // add bool return type?
-	var body Login // remove this?
-	if c.BindJSON(&body) == nil {
-		db := c.MustGet("db").(*sql.DB)
-		username, password, hasAuth := c.Request.BasicAuth()
-		users := QueryDb(db, "select * from user where username = ?", username)
-
-		if len(users) == 0 {
-			c.JSON(http.StatusNotFound, gin.H{"status": "Invalid username"})
-		} else if !pwdHash.CheckPasswordHash(password, users[0]["pw_hash"].(string)) {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "password is incorrect"})
-		} else if hasAuth {
-			c.JSON(http.StatusNoContent, nil)
-			return
-		} else {
-			c.JSON(http.StatusUnauthorized, gin.H{"status": "Couldn't authenticate"}) // // c.Writer.Header().Set("WWW-Authenticate", "Basic realm=Restricted")
-			c.Abort()
-			return
-		}
+func IsAuthenticated(c *gin.Context) bool {
+	db := c.MustGet("db").(*sql.DB)
+	username, password, hasAuth := c.Request.BasicAuth()
+	if !hasAuth {
+		return false
 	}
+	users := QueryDb(db, "select * from user where username = ?", username)
+
+	if len(users) == 0 || !pwdHash.CheckPasswordHash(password, users[0]["pw_hash"].(string)) {
+		return false
+	}
+	return true
 }
