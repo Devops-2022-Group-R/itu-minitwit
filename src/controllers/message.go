f51b7fe3530239791b22ab7c131494c1211909b7
Joachim Borup <joachim@borup.com>
2022-02-19 15:57:37 +0000

    Remove unused code from message controller

--- src/controllers/message.go
+++ src/controllers/message.go
@@ -9,9 +9,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type MessageRequestBody struct {
-	Username string `form:"username" json:"username" binding:"required"`
-	Content  string `form:"content" json:"content" binding:"required"`
+type PostUserMessageRequestBody struct {
+	Content string `form:"content" json:"content" binding:"required"`
 }
 
 func GetMessages(c *gin.Context) {
@@ -50,15 +49,10 @@ func GetUserMessages(c *gin.Context) {
 }
 
 func PostUserMessage(c *gin.Context) {
-	var body MessageRequestBody
+	var body PostUserMessageRequestBody
 
 	if err := c.BindJSON(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	if body.Content == "" {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "Message may not be empty"})
 		return
 	}
 
@@ -85,36 +79,3 @@ func PostUserMessage(c *gin.Context) {
 
 	c.JSON(http.StatusNoContent, nil)
 }
-
-/*
-// Registers a new message for the user.
-func addMessage(c *gin.Context) {
-	user, userLoggedIn := c.Get("user")
-
-	if !userLoggedIn {
-		c.JSON(401, nil)
-		return
-	}
-
-	err := c.Request.ParseForm()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	form := c.Request.Form
-	text := form.Get("text")
-
-	if text != "" {
-		messageRepository := c.MustGet(messageRepositoryKey).(database.IMessageRepository)
-		messageRepository.Create(models.Message{
-			Author:  user.(models.User),
-			Text:    text,
-			PubDate: time.Now().Unix(),
-		})
-
-		flash(c, "Your message was recorded")
-	}
-
-	c.Redirect(302, timeLineUrl)
-}
-*/
