65e2deef0751f91b79ac02d1cfcf9862af744401
Adrian Borup <adrian@borup.com>
2022-02-19 18:13:02 +0000

    Merge unfollow and follow controllers

--- src/controllers/follow.go
+++ src/controllers/follow.go
@@ -9,7 +9,8 @@ import (
 )
 
 type FollowRequestBody struct {
-	Follow string `form:"follow" json:"follow" binding:"required"`
+	Follow   string `form:"follow" json:"follow"`
+	Unfollow string `form:"unfollow" json:"unfollow"`
 }
 
 // Adds the current user as follower of the given user.
@@ -35,19 +36,44 @@ func FollowPostController(c *gin.Context) {
 	// TODO: check if requestee = username in url. Or ignore requestee and use value from auth
 	// TODO: forbidden if not logged in
 
-	target, err := userRepository.GetByUsername(body.Follow)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	} else if target == nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "the username to follow does not exist"})
-		return
+	var followTargetUserId int64
+	if len(body.Follow) > 0 {
+		followTarget, err := userRepository.GetByUsername(body.Follow)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		} else if followTarget == nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "the username to follow does not exist"})
+			return
+		}
+		followTargetUserId = followTarget.UserId
 	}
 
-	err = userRepository.Follow(author.UserId, target.UserId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
+	var unfollowTargetUserId int64
+	if len(body.Unfollow) > 0 {
+		unfollowTarget, err := userRepository.GetByUsername(body.Unfollow)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		} else if unfollowTarget == nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "the username to unfollow does not exist"})
+			return
+		}
+		unfollowTargetUserId = unfollowTarget.UserId
+	}
+
+	if len(body.Follow) > 0 {
+		if err = userRepository.Follow(author.UserId, followTargetUserId); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	if len(body.Unfollow) > 0 {
+		if err = userRepository.Unfollow(author.UserId, unfollowTargetUserId); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 	}
 
 	c.JSON(http.StatusNoContent, nil)
@@ -80,49 +106,4 @@ func FollowGetController(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"follows": usernames,
 	})
-}
-
-type UnfollowRequestBody struct {
-	Unfollow string `form:"unfollow" json:"unfollow" binding:"required"`
-}
-
-// Removes the current user as follower of the given user.
-func UnfollowController(c *gin.Context) {
-	userRepository := c.MustGet(UserRepositoryKey).(database.IUserRepository)
-
-	var body UnfollowRequestBody
-	if err := c.BindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	urlUsername := c.Param("username")
-	author, err := userRepository.GetByUsername(urlUsername)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	} else if author == nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "the username provided in the URL does not exist"})
-		return
-	}
-
-	// TODO: check if requestee = username in url. Or ignore requestee and use value from auth
-	// TODO: forbidden if not logged in
-
-	target, err := userRepository.GetByUsername(body.Unfollow)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	} else if target == nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "the username to unfollow does not exist"})
-		return
-	}
-
-	err = userRepository.Unfollow(author.UserId, target.UserId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusNoContent, nil)
 }
