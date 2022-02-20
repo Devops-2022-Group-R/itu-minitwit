0aa3b63af91428f45bb9d11a8642b33a895f4018
ThomasITU <thwr@itu.dk>
2022-02-20 15:32:35 +0000

    Add feedTest NotFound and Feed messages
    Co-authored-by: Albert Rise Nielsen <albert@risenielsen.dk>

--- src/test/controllers/message_test.go
+++ src/test/controllers/message_test.go
@@ -3,11 +3,15 @@ package controllers_test
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/Devops-2022-Group-R/itu-minitwit/src/models"
 )
 
 func (suite *TestSuite) Test_GetMessages_Returns_OK() {
@@ -94,6 +98,86 @@ func (suite *TestSuite) Test_PostUserMessage_Given_Simulator_Returns_No_Content(
 
 	// Assert
 	assert.Equal(suite.T(), http.StatusBadRequest, w.Code)
+}
+
+func (suite *TestSuite) Test_GetFeedMessages_Given_NonExistent_User_Returns_NotFound() {
+	// Arrange
+	encodedCredentials := encodeCredentialsToB64("test", "1234")
+	req := httptest.NewRequest(http.MethodGet, "/feed", nil)
+	req = setHeaderContent(req, encodedCredentials)
+
+	// Act
+	w := suite.sendRequest(req)
+
+	// Assert
+	assert.Equal(suite.T(), http.StatusNotFound, w.Code)
+}
+
+//
+func (suite *TestSuite) Test_GetFeedMessages_Given_ValidUser_Returns_Ok() {
+	// Arrange
+	suite.registerUser("Testing", "tester@eh.com", "Testy")
+	encodedCredentials := encodeCredentialsToB64("Testing", "Testy")
+	req := httptest.NewRequest(http.MethodGet, "/feed", nil)
+	req = setHeaderContent(req, encodedCredentials)
+
+	// Act
+	w := suite.sendRequest(req)
+
+	// Assert
+	assert.Equal(suite.T(), http.StatusOK, w.Code)
+}
+
+// messages from self and followers
+func (suite *TestSuite) Test_GetFeedMessages_Given_ValidUser_FollowingOtherUsers_returns_Messages() {
+	// Arrange
+	suite.registerUser("Testing", "tester@eh.com", "Testy")
+	suite.registerUser("Jennifer", "jenni@eh.com", "Jesty")
+	suite.registerUser("Geralt", "WhiteWolf@eh.com", "Gesty")
+
+	testyCredentials := encodeCredentialsToB64("Testing", "Testy")
+	gestyCredentials := encodeCredentialsToB64("Geralt", "Gesty")
+	jestyCredentials := encodeCredentialsToB64("Jennifer", "Jesty")
+
+	geraltMsg, _ := json.Marshal(gin.H{"content": "Gesty msg"})
+	reqG := httptest.NewRequest(http.MethodPost, "/msgs/Geralt", bytes.NewReader(geraltMsg))
+	reqG = setHeaderContent(reqG, gestyCredentials)
+	suite.sendRequest(reqG)
+
+	jenniferMsg, _ := json.Marshal(gin.H{"content": "Jesty msg"})
+	reqJ := httptest.NewRequest(http.MethodPost, "/msgs/Jennifer", bytes.NewReader(jenniferMsg))
+	reqJ = setHeaderContent(reqJ, jestyCredentials)
+	suite.sendRequest(reqJ)
+
+	time.Sleep(1 * time.Second)
+
+	testyMsg, _ := json.Marshal(gin.H{"content": "Testy msg"})
+	reqT := httptest.NewRequest(http.MethodPost, "/msgs/Testing", bytes.NewReader(testyMsg))
+	reqT = setHeaderContent(reqT, testyCredentials)
+	suite.sendRequest(reqT)
+
+	fllwJennifer, _ := json.Marshal(gin.H{"follow": "Jennifer"})
+	reqT = httptest.NewRequest(http.MethodPost, "/fllws/Testing", bytes.NewReader(fllwJennifer))
+	reqT = setHeaderContent(reqT, testyCredentials)
+	suite.sendRequest(reqT)
+
+	req := httptest.NewRequest(http.MethodGet, "/feed", nil)
+	req = setHeaderContent(req, testyCredentials)
+
+	msg0 := models.Message{Text: "Testy msg",}
+	msg1 := models.Message{Text: "Jesty msg",}
+
+	// Act
+	w := suite.sendRequest(req)
+	var resBody []models.Message
+	resBodyBytes, _ := ioutil.ReadAll(w.Result().Body)
+	json.Unmarshal(resBodyBytes, &resBody)
+
+	// Assert
+	assert.Equal(suite.T(), http.StatusOK, w.Code)
+	assert.Equal(suite.T(), 2, len(resBody))
+	assert.Equal(suite.T(), msg0.Text, resBody[0].Text)
+	assert.Equal(suite.T(), msg1.Text, resBody[1].Text)
 }
 
 func (suite *TestSuite) registerUser(username, email, password string) {
