5faf885e0d37ce09d300c8f8bf3ab7452f8d490c
Adrian Borup <adrian@borup.com>
2022-02-20 13:53:50 +0000

    Create simulator user during database init

--- src/database/database.go
+++ src/database/database.go
@@ -3,6 +3,7 @@ package database
 import (
 	"log"
 
+	"github.com/Devops-2022-Group-R/itu-minitwit/src/models"
 	"gorm.io/gorm"
 )
 
@@ -26,7 +27,17 @@ func InitDatabase(openDatabase OpenDatabaseFunc) {
 		log.Fatal(err)
 	}
 
-	NewGormUserRepository(db).Migrate()
+	userRepository := NewGormUserRepository(db)
+	userRepository.Migrate()
 	NewGormMessageRepository(db).Migrate()
 	NewGormLatestRepository(db).Migrate()
+
+	// The simulator needs to be a default user
+	if err = userRepository.Create(models.User{
+		Username:     "simulator",
+		Email:        "unused@email.rip",
+		PasswordHash: "pbkdf2:sha256:260000$MYtvPfLTCXY74kXG$44274a66ed9a6d08471cfc77337ae10497ba3c4a4cb4adec02227305ce663378",
+	}); err != nil {
+		log.Fatal(err)
+	}
 }
