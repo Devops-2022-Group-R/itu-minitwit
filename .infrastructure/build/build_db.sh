fc4dc50c3e51454b2ae4bb8715612c2f7237a3ec
albe2669 <albert@risenielsen.dk>
2022-02-22 01:18:46 +0100

    Fix db script

--- .infrastructure/build/build_db.sh
+++ .infrastructure/build/build_db.sh
@@ -3,14 +3,22 @@
 sudo apt-get update
 sudo apt-get install -y postgresql postgresql-contrib
 
-sudo -u postgres psql -c "CREATE USER vagrant WITH ENCRYPTED PASSWORD 'vagrant';"
-sudo -u postgres psql -c "CREATE DATABASE vagrant;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE vagrant to vagrant;"
+db="vagrant"
+user="vagrant"
+password="vagrant"
+
+sudo -u postgres psql -c "CREATE USER $user WITH ENCRYPTED PASSWORD '$password';"
+sudo -u postgres psql -c "CREATE DATABASE $db;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $db to $user;"
 
 export configPath=$(sudo -u postgres psql -c 'SHOW config_file' 2>/dev/null | grep /etc)
 export hbaPath=$(sudo -u postgres psql -c 'SHOW hba_file' 2>/dev/null | grep /etc)
 
-echo "listen_addresses = '*'" | sudo tee -a $configPath
-echo "host samerole all 0.0.0.0/0 md5" | sudo tee -a $hbaPath
+localNetwork=$(ip route | awk '/.* eth1 .*/ {print $1}')
+ip=$(ip route | awk '/.* eth1 .*/ {print $NF}')
+
+echo "listen_addresses = '$ip'" | sudo tee -a $configPath
+# Type database user address method
+echo "host $db $user $localNetwork md5" | sudo tee -a $hbaPath
 
 sudo service postgresql restart
