7b7cf9aef58b2fefe306f37cc40e1e5fb83b4d3b
Adrian Borup <adrian@borup.com>
2022-02-22 19:56:28 +0000

    Fix app service name in Terraform

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "backend_as" {
-  name                = "${var.prefix}-backend_as"
+  name                = "${var.prefix}-backend-as"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
