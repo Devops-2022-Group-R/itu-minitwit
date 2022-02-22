bf48744074209b697aae6d740ae0e2a85914473f
Adrian Borup <adrian@borup.com>
2022-02-22 19:29:04 +0000

    Set up app service for server with Terraform

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -15,6 +15,36 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "itu-minitwit-rg"
+  name     = "${var.prefix}-rg"
   location = "northeurope"
+}
+
+resource "azurerm_app_service_plan" "asp" {
+  name                = "${var.prefix}-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B2"
+  }
+}
+
+resource "azurerm_app_service" "backend_as" {
+  name                = "${var.prefix}-backend_as"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    app_command_line = ""
+    linux_fx_version = "DOCKER|kongborup/itu-minitwit-server:latest"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://registry.hub.docker.com"
+  }
 }
