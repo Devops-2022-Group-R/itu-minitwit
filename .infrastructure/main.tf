a011222b5eff29d5541b3f1c765667f57bf085f0
Adrian Borup <adrian@borup.com>
2022-02-22 20:48:33 +0000

    Enable HTTPS only in Terraform

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -37,6 +37,7 @@ resource "azurerm_app_service" "backend_as" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
+  https_only          = true
 
   site_config {
     app_command_line = ""
