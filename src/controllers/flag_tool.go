bb3deade967dc49df6d6cfc59df180acb891d130
ThomasITU <thwr@itu.dk>
2022-03-04 15:05:18 +0000

    add FlagMessageById

--- src/controllers/flag_tool.go
+++ src/controllers/flag_tool.go
@@ -1,8 +1,24 @@
 package controllers
 
 import (
+	"net/http"
+	"strconv"
+
+	"github.com/Devops-2022-Group-R/itu-minitwit/src/database"
 	"github.com/gin-gonic/gin"
 )
+
+func FlagMessageById(c *gin.Context) {
+	messageRepository := c.MustGet(MessageRepositoryKey).(database.IMessageRepository)
+	msgId, _ := strconv.Atoi(c.Param("msgid"))
+	err := messageRepository.FlagByMsgId(msgId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
+}
 
 func GetAllMessages(c *gin.Context) {
 	messageRepository := c.MustGet(MessageRepositoryKey).(database.IMessageRepository)
