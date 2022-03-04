ef8f0f3bffda0cacab5d6432337b8978d98452ea
ThomasITU <thwr@itu.dk>
2022-03-04 15:04:26 +0000

    add GetAllMessages

--- src/controllers/flag_tool.go
+++ src/controllers/flag_tool.go
@@ -4,6 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetMessageById(c *gin.Context) {
+func GetAllMessages(c *gin.Context) {
+	messageRepository := c.MustGet(MessageRepositoryKey).(database.IMessageRepository)
+	messages, err := messageRepository.GetWithLimit(perPage)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
+	c.JSON(http.StatusOK, messages)
 }
