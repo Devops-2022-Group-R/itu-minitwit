4ccd26d6c4573d0bf1a6d390b8ae4346d8f26742
ThomasITU <thwr@itu.dk>
2022-03-13 10:52:30 +0000

    add cpu load

--- src/monitoring/monitoring.go
+++ src/monitoring/monitoring.go
@@ -1,7 +1,10 @@
 package monitoring
 
 import (
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/shirou/gopsutil/cpu"
 )
 
 var cpuTemp = prometheus.NewGauge(prometheus.GaugeOpts{
@@ -9,6 +12,19 @@ var cpuTemp = prometheus.NewGauge(prometheus.GaugeOpts{
 	Help: "Current temperature of the CPU.",
 })
 
+var cpuLoad = prometheus.NewGauge(prometheus.GaugeOpts{
+	Name: "cpu_load_percentage",
+	Help: "Current cpu load in percentage",
+})
+
 func Initialise() {
 	prometheus.MustRegister(cpuTemp)
+	prometheus.MustRegister(cpuLoad)
+	go func() {
+		for {
+			cpuLoadTemp, _ := cpu.Percent(time.Second, false)
+			cpuLoad.Set(cpuLoadTemp[0])
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
 }
