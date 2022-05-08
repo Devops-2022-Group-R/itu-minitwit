99dbff89f410464e7bbe08aa2f67d1258dff2483
ThomasITU <thwr@itu.dk>
2022-05-08 19:33:58 +0000

    attempt to fix missing permission

--- .circleci/check_latex.sh
+++ .circleci/check_latex.sh
@@ -1,14 +1,14 @@
-# With inspiration from: https://gist.github.com/naesheim/18d0c0a58ee61f4674353a2f4cf71475
+#!/bin/bash
 
 set -e
-
+# With inspiration from: https://gist.github.com/naesheim/18d0c0a58ee61f4674353a2f4cf71475
 # latest commit
 LATEST_COMMIT=$(git rev-parse HEAD)
 
 # latest commit where path/to/folder1 was changed
 REPORT_COMMIT=$(git log -1 --format=format:%H --full-diff /report)
 
-if [ $REPORT_COMMIT = $LATEST_COMMIT ];
+if [ "$REPORT_COMMIT" = "$LATEST_COMMIT" ];
     then
         echo "files in Report has changed"
         cd /report
