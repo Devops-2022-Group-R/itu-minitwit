d17fc6e300e980a5201e36d0caffe1487b539a16
Thomas <thwr@itu.dk>
2022-05-10 13:49:52 +0200

    git

--- report/scripts/compile-latex-docker.sh
+++ report/scripts/compile-latex-docker.sh
@@ -7,5 +7,11 @@ docker run \
   -i \
   --net=none \
   -v $(pwd):/data \
-  alpine:latest \
-  ls /data 
+  kongborup/custom-latex:latest \
+  pdflatex \
+  --shell-escape \
+  -interaction=nonstopmode \
+  -file-line-error \
+  -output-directory=build \
+  -aux-directory=build \
+  $1
